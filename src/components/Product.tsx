099f45c9d6707142d5cec2ec1965684d3ec0f275
Y031k <bmbacardi@gmail.com>
2025-04-13 21:03:00 +0300

    First revision

---
+++ src/components/Product.tsx
@@ -0,0 +1,46 @@
+import './Product.scss'
+
+
+/*   local currency format */
+export const moneyFormatter = Intl.NumberFormat('en-ES', {
+	currency: 'USD',
+	currencyDisplay: 'symbol',
+	currencySign: 'standard',
+	style: 'currency',
+	minimumFractionDigits: 2,  
+	maximumFractionDigits: 4,  
+	minimumIntegerDigits: 1,   
+ });
+
+/*   product interfatce */
+
+interface ProductCard {
+	imgsrc: string
+	title: string
+	descrption: string
+	price: number
+}
+
+function Product({imgsrc, title, descrption, price}: ProductCard) {
+	return (
+	  <>
+			<div className="card">
+				<div className="card__body">
+					<img src={imgsrc}/>
+					<h2 className="card__title">{title}</h2>
+					<p className="card__description">{descrption}</p>
+					<h2 className="card__price">{moneyFormatter.format(price)}</h2>
+					<button onClick={
+						()=>{
+						console.log( 'Product', {title}, 'added to cart')
+						}
+					}className="card__btn">
+					Add to basket
+					</button>
+				</div>
+			</div>
+	  </>
+	)
+ }
+ 
+ export default Product
---
+++ src/main.tsx
@@ -0,0 +1,34 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.scss'
+import Product from './components/Product.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+	<div className="wrapper">
+		<Product
+				imgsrc = "https://orebishopping.reactbd.com/static/media/spfFour.d005441cba54d7214bf7.webp"
+				title = "Sun glasses"
+				descrption = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+				price = {200.00}
+		/>
+		<Product
+				imgsrc="https://orebishopping.reactbd.com/static/media/newArrFour.b8a5fda6839f2f0b1eaa.webp"
+				title="Funny Toy"
+				descrption="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+				price = {60.00}
+		/>
+				<Product
+				imgsrc="https://orebishopping.reactbd.com/static/media/bestSellerTwo.c2641a76c189fcc71670.webp"
+				title="Backpack"
+				descrption="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+				price = {120.00}	
+		/>
+
+
+
+
+
+	</div> 
+  </StrictMode>,
+)
