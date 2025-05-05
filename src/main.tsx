60a6f8e7d35e14a1997c4ed94d96e9eaf00bde14
Y031k <bmbacardi@gmail.com>
2025-05-05 21:17:55 +0300

    tinyAdjustments

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './main.scss'
-import Product from './components/Product.tsx'
+import Product from './components/productCard/Product.tsx'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
