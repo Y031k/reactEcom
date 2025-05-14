6f244bbe9a0882a4689e37c6fc707abee1dc9e36
Y031k <bmbacardi@gmail.com>
2025-05-14 09:13:21 +0300

    Test commit

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -4,7 +4,7 @@ import './button.css';
 
 
 function Button() {
-	return <button className="primary lg">&#129120; &#160; Button &#160;&#129122;</button>;
+	return <button className="primary tn">&#129120; &#160; Button &#160;&#129122;</button>;
 }
 
 export default Button
